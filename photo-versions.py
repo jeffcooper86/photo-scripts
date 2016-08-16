c6dc2999422be0cf7f191873ebdeb19377f777a8
Jeffrey Cooper <jeffcooperjc@gmail.com>
2016-08-16 01:16:08 -0400

    Initial functionality for saving a small preview image and saving an image for specified print ratio with a white border.

--- photo-versions.py
+++ photo-versions.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
-image_sizes = [
+# Settings.
+print_sizes = [
     (5, 7),
     (8, 10),
     (10, 12),
@@ -8,29 +9,66 @@ image_sizes = [
     (16, 20),
     (20, 30)]
 
-# Open original image
-original_image = Image.open('glacier/IMG_8962 (2).jpg')
-original_size = original_image.size
+small_size = (667, 1000)
 
-# 300 dpi prints
-print_size = tuple(300 * x for x in image_sizes[0])
+# Helper functions.
+def get_image_size_with_border(print_size, border_ratio):
 
-if original_size[0] > original_size[1]:
-     print_size = tuple(reversed(print_size))
+    # Preserve 2:3 aspect ratio
+    long_side = max(print_size[0], print_size[1]) * (1 - border_ratio)
+    short_side = 2.0 / 3 * long_side
+    return (int(long_side), int(short_side))
 
-# Resize original image for print size with a border
-picture_size = tuple(int(0.75 * x) for x in print_size)
-print picture_size
-resized_image = original_image.resize(picture_size, Image.ANTIALIAS)
+def get_orientation(image):
+    size = image.size
+    if size[0] == size[1]:
+        return 'square'
+    elif size[0] > size[1]:
+        return 'landscape'
+    return 'portrait'
 
-# Place resized image in new image with border
-border_size = (
-    (print_size[0] - picture_size[0]) / 2,
-    (print_size[1] - picture_size[1]) / 2
+def get_print_size_in_pixels(ratio_in_inches):
+
+    # 300 dpi.
+    print_size = tuple(300 * x for x in ratio_in_inches)
+
+    # Match the print orientation to the image. Square defaults to portrait.
+    if get_orientation(original_image) == 'landscape':
+        print_size = tuple(reversed(print_size))
+    return print_size
+
+def make_print_with_border(image, print_size):
+    border_color = (255, 255, 255)
+    border_ratio = 0.25
+    bordered_image_size = get_image_size_with_border(print_size, border_ratio)
+    resized_image = resize_image(image, bordered_image_size)
+    image_offset_for_border = (
+        (print_size[0] - bordered_image_size[0]) / 2,
+        (print_size[1] - bordered_image_size[1]) / 2
     )
 
-new_image = Image.new("RGB", print_size, (255, 255, 255))
-new_image.paste(resized_image, border_size)
+    # Make a new image and paste the resized image in it.
+    bordered_image = Image.new("RGB", print_size, border_color)
+    bordered_image.paste(resized_image, image_offset_for_border)
+    return bordered_image
 
-new_image.show()
-# new_image.save('test.jpg')
+def resize_image(image, size):
+    return image.resize(size, Image.ANTIALIAS)
+
+def save_bordered_image(image, print_size):
+    print_size = get_print_size_in_pixels(print_size)
+    make_print_with_border(image, print_size).save('dist/bordered_image.jpg')
+
+def save_small_image(image, small_size):
+    if get_orientation(image) == 'landscape':
+        small_size = tuple(reversed(small_size))
+    resize_image(original_image, small_size).save('dist/small.jpg')
+
+# Get the image.
+original_image = Image.open('sample/IMG_8962 (2).jpg')
+
+# Save a small version for previews.
+save_small_image(original_image, small_size)
+
+# Save bordered images for prints.
+save_bordered_image(original_image, print_sizes[1])
